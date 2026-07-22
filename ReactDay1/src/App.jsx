c011386ebfde2dadb525c02668491bad0321d911
Bibek Yadav <bibekyadav735@gmail.com>
2026-07-22 23:14:42 +0545

    React Props

--- ReactDay1/src/App.jsx
+++ ReactDay1/src/App.jsx
@@ -1,23 +1,46 @@
 import React from 'react'
+import Product from './Product';
+
 
 const App = () => {
 
-  function checkResult() {
-    let marks = prompt("Enter marks:")
-    if (marks >= 40) {
-      alert("Pass")
-    } else {
-      alert("Fail")
+  const employee = [
+    {
+      name: "Raj",
+      salary: 20000
+    },
+    {
+      name: "Ajay",
+      salary: 30000
     }
-  }
+  ];
+
+  const mapEmployee = employee.map((item) => `${item.name}, ${item.salary}`);
+  const totalSalary = employee.reduce((acc, item) => acc + item.salary, 0);
 
   return (
     <div>
-      <button onClick={checkResult}>Check Result</button>
+      {mapEmployee.map((str, index) => (
+        <p key={index}>{str}</p>
+      ))}
+
+      {employee
+        .filter((item) => item.salary > 20000)
+        .map((item, index) => (
+          <p key={index}>{item.name}, {item.salary}</p>
+        ))}
+
+      <p className='text-amber-300'>Total salary: {totalSalary}</p>
+
+
+      <Product title="Laptop" price={55000} />
+      <Product title="Phone" price={25000} />
+
+
     </div>
   )
-}
+};
+
 
 export default App
 
-// see filter 
