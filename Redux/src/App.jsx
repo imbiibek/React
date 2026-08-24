25987322580624e43764e32f78d036bddf7f3e66
Bibek Yadav <bibekyadav735@gmail.com>
2026-08-24 16:45:06 +0545

    Update

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {useDispatch, useSelector } from 'react-redux'
 
 const App = () => {
+
+const dispatch = useDispatch()
+const count = useSelector() 
+
   return (
-    <div>App</div>
+    <div>
+
+<h1>0</h1>
+
+<button onClick={() => {
+
+}}>Increment</button>
+
+<button onClick={() => {
+
+}}>Decrement</button>
+
+    </div>
   )
 }
 
