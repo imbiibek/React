c324b15229d1363b6501e841467e74edfe767122
Bibek Yadav <bibekyadav735@gmail.com>
2026-09-22 10:23:03 +0545

    useState

--- Reactpractice/src/App.jsx
+++ Reactpractice/src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import Form from './components/Form';
-
-
-var userIsRegistered = true;
+import React, { useState } from 'react'
 
 const App = () => {
-  return (
-    
-    <div className="container">
-      <Form isRegistered={userIsRegistered} />
-    </div>
 
+  const [state, setState] = useState(0)
+
+  function increase() {
+    setState(state + 1)
+  }
+
+  const decrease = () => setState(state - 1);
+
+
+  return (
+    <div className="bg-red-500 text-white text-3xl p-10" >
+
+      <h1 className="text-6xl">{state}</h1>
+
+      <button onClick={increase} className="text-4xl">+</button>
+      <button onClick={decrease} className="text-7xl">-</button>
+
+
+
+    </div>
   )
 }
 
